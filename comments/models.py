7ad2d6faf1d9e99b73887471a997d47da7b51f84
Eddie Kao <eddie@5xcampus.com>
2025-08-12 11:50:40 +0800

    feat: add user to comment

--- comments/models.py
+++ comments/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from articles.models import Article
 from datetime import datetime
+from django.contrib.auth.models import User
 
 
 class Comment(models.Model):
     content = models.TextField(null=False)
     article = models.ForeignKey(Article, default=None, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, default=None, null=True, on_delete=models.SET_NULL)
 
     created_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(null=True, db_index=True)
--- comments/views.py
+++ comments/views.py
@@ -5,23 +5,27 @@ from articles.models import Article
 from django.contrib import messages
 from django.views.decorators.http import require_POST, require_http_methods
 from .models import Comment
+from django.contrib.auth.decorators import login_required
 
 
 @require_POST
+@login_required
 def create(request, id):
     article = get_object_or_404(Article, pk=id)
 
     form = CommentForm(request.POST)
     comment = form.save(commit=False)
     comment.article = article
+    comment.user = request.user
     comment.save()
 
     return render(request, "comments/comment.html", {"comment": comment})
 
 
 @require_http_methods(["DELETE"])
+@login_required
 def delete(request, id):
-    comment = get_object_or_404(Comment, pk=id)
+    comment = get_object_or_404(Comment, pk=id, user=request.user)
     comment.delete()
 
     return HttpResponse("")
